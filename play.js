bace3265e3c920c32d610afda56c32c5a5f03fc4
William Kemeny <williamkemeny@gmail.com>
2023-05-16 22:19:10 +0000

    Created play.js to take in data from the server

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", (connect) => {
+    console.log("Connected to Snek");
+    conn.write("Name: WIL");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
