6914acd746a60f623cdf85ad035aaf021c41463a
William Kemeny <williamkemeny@gmail.com>
2023-05-18 09:59:55 -0400

    created input.js folder

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === "\u0003") {
+    process.stdout.write("Disconnecting...\n");
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const { connect } = require("./client");
-
-const setupInput = function (connection) {
-  conn = connection;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.stdout.write("Disconnecting...\n");
-    process.exit();
-  }
-};
+const { setupInput } = require("./input");
 
 setupInput(connect);
 
